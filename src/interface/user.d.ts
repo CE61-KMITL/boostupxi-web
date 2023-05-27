2432a25417554ee32f8f724770d4984fcefa563d
khris-xp <khrisbharmmano@gmail.com>
2023-05-27 18:14:18 +0700

    feat: profile page

--- src/interface/user.d.ts
+++ src/interface/user.d.ts
@@ -1,7 +1,11 @@
 export interface IUser {
+    _id: string;
     email: string;
     username: string;
     score: number;
     role: string;
+    group: string;
+    createdAt: string;
+    updatedAt: string;
     tasks: never[];
 }
