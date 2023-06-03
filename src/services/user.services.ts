4d553242f35b444def64e1e3f0c626a1c11c697b
Kittipod Lambangchang <tonkittipod@gmail.com>
2023-06-03 17:36:39 +0700

    refactor: don't pass role in request body (updateProfile service)

--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -1,49 +1,49 @@
-import Cookies from "js-cookie";
-import { axiosInstance } from "./api.services";
+import Cookies from 'js-cookie';
+import { axiosInstance } from './api.services';
 
 export const userService = {
-    login: async (email: string, password: string) => {
-        try {
-            const response = await axiosInstance.post("/auth/login", {
-                email,
-                password,
-            });
-            const token = response.headers.authorization;
-            Cookies.set("token", token);
-            setTimeout(() => {
-                window.location.href = '/question';
-            }, 1000)
-        } catch (error) {
-            const message = (error as Error).message;
-            throw new Error(message);
-        }
-    },
-    logout: async () => {
-        try {
-            Cookies.remove('token');
-            setTimeout(() => {
-                window.location.href = '/';
-            }, 1000)
-        } catch (error) {
-            const message = (error as Error).message;
-            throw new Error(message);
-        }
-    },
-    getUserProfile: async () => {
-        const response = await axiosInstance.get("/users/profile");
-        return response.data;
-    },
-    editUserProfile: async (id: string, username: string, passsword: string, role: string) => {
-        try {
-            const response = await axiosInstance.patch(`/users/${id}`, {
-                username,
-                passsword,
-                role,
-            });
-            return response.data;
-        } catch (error) {
-            const message = (error as Error).message;
-            throw new Error(message);
-        }
-    }
-}
+	login: async (email: string, password: string) => {
+		try {
+			const response = await axiosInstance.post('/auth/login', {
+				email,
+				password
+			});
+			const token = response.headers.authorization;
+			Cookies.set('token', token);
+			setTimeout(() => {
+				window.location.href = '/question';
+			}, 1000);
+		} catch (error) {
+			const message = (error as Error).message;
+			throw new Error(message);
+		}
+	},
+	logout: async () => {
+		try {
+			Cookies.remove('token');
+			setTimeout(() => {
+				window.location.href = '/';
+			}, 1000);
+		} catch (error) {
+			const message = (error as Error).message;
+			throw new Error(message);
+		}
+	},
+	getUserProfile: async () => {
+		const response = await axiosInstance.get('/users/profile');
+		return response.data;
+	},
+	editUserProfile: async (id: string, username: string, password: string) => {
+		try {
+			console.log(id, username, password);
+			const response = await axiosInstance.patch(`/users/${id}`, {
+				username,
+				password
+			});
+			return response.data;
+		} catch (error) {
+			const message = (error as Error).message;
+			throw new Error(message);
+		}
+	}
+};
