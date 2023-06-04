2c60167e610ba03f191cf53a7da3d6b3699b0979
khris-xp <khrisbharmmano@gmail.com>
2023-06-04 09:45:51 +0700

    refactor: constants

--- src/store/submission.ts
+++ src/store/submission.ts
@@ -1,33 +1,7 @@
+import { initialSubmissionData, initialSubmissionResult } from "$/constants/submission.constants";
 import type { ISubmissionsResult } from "$/interface/submission";
 import { questionService } from "$/services/question.services";
 import { writable, type Writable } from "svelte/store";
-
-export const initialSubmissionData: ISubmissionsResult = {
-    _id: "",
-    question: "",
-    user: "",
-    __v: 0,
-    createdAt: "",
-    result: "",
-    updatedAt: "",
-    source_code: "",
-    status: false,
-}
-
-export const initialSubmissionResult: string[] = [
-    "C: Cannot Create File from Source Code",
-    "L: Banned Library Usage,",
-    "F: Banned Function Usage",
-    "H: Hacker ",
-    "W: Testcase Error",
-    "S: Syntax Error",
-    "T: Timeout",
-    "O: Out of Buffer",
-    "R: Runtime Error",
-    "E: General Error",
-    "P: Passed",
-    "-: Failed",
-]
 
 export const submissionResultStore: Writable<string[]> = writable(initialSubmissionResult);
 
