abf68c0604b7e3085bc4256f3d7a669241f9eb06
khris-xp <khrisbharmmano@gmail.com>
2023-06-05 12:03:44 +0700

    feat: submit code without reload

--- src/store/submission.ts
+++ src/store/submission.ts
@@ -4,10 +4,13 @@ import { questionService } from "$/services/question.services";
 import { writable, type Writable } from "svelte/store";
 
 export const submissionResultStore: Writable<string[]> = writable(initialSubmissionResult);
-
 export const submissionDataStore: Writable<ISubmissionsResult> = writable(initialSubmissionData);
 
-export const updateSubmissionData = async (id: string): Promise<void> => {
+export const updateSubmissionData = async (id: string) => {
     const response = await questionService.getSubmission(id);
+    submissionDataStore.set(response);
+}
+
+export const updateSubmissionResponse = async (response: ISubmissionsResult) => {
     submissionDataStore.set(response);
 }
