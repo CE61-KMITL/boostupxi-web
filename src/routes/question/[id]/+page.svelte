29b1a2d116ae2656d9f1a0990372c30f4a1b6b6d
1tpp <panapat.dev@gmail.com>
2023-06-17 13:30:28 +0700

    feat: zero level

--- src/routes/question/[id]/+page.svelte
+++ src/routes/question/[id]/+page.svelte
@@ -58,21 +58,27 @@
 							<h1 class=" leading-[80px] text-3xl font-bold overflow-hidden">
 								{question?.title}
 							</h1>
-							<div class="flex">
-								{#each Array(question?.level) as _, i}
-									<svg
-										aria-hidden="true"
-										class="w-9 h-9 text-yellow-300"
-										fill="currentColor"
-										viewBox="0 0 20 20"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<path
-											d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-										/>
-									</svg>
-								{/each}
-							</div>
+							{#if question?.level > 0}
+								<div class="flex">
+									{#each Array(question?.level) as _, i}
+										<div class="">
+											<svg
+												aria-hidden="true"
+												class="w-9 h-9 text-yellow-300"
+												fill="currentColor"
+												viewBox="0 0 20 20"
+												xmlns="http://www.w3.org/2000/svg"
+											>
+												<path
+													d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+												/>
+											</svg>
+										</div>
+									{/each}
+								</div>
+							{:else}
+								<span class="h-9 text-gray-300">(ไม่ได้รับคะแนน)</span>
+							{/if}
 						</div>
 						<div class="flex justify-between">
 							<h2 class="flex text-sm tracking-widest">
