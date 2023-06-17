1c80f921a6411e54db103846f91b2e1d3e209b3c
1tpp <panapat.dev@gmail.com>
2023-06-17 08:05:47 +0700

    style: update

--- src/routes/guide/+page.svelte
+++ src/routes/guide/+page.svelte
@@ -1,19 +1,26 @@
-<div class="2xl:container 2xl:mx-auto lg:py-16 lg:px-20 md:py-12 md:px-6 py-9 px-4 text-white">
-	<h2 class="w-11/12 font-bold text-2xl">About</h2>
-	<div class="flex lg:flex-row flex-col-reverse justify-around lg:items-start items-center">
-		<div class="w-11/12 lg:w-1/2 mt-10 xl:mt-0">
-			<h2 class="w-full font-bold text-4xl mt-3">CE BoostUp-XI Grader</h2>
-			<p class="font-normal text-base leading-6 mt-6">
+<div class="container mx-auto pt-20 text-white">
+	<div class="grid grid-cols-1 lg:grid-cols-2 gap-2 justify-center items-center">
+		<div class="flex flex-col justify-center glass-lightgray p-8">
+			<h2 class="w-full font-bold text-4xl mt-3 uppercase">CE BoostUp XI</h2>
+			<p class="font-normal text-gray-300 leading-6 mt-6">
 				สวัสดีครับน้องๆที่กำลังอ่านข้อความนี้ ค่ายนี้ก็เป็นครั้งที่ 11 แล้วนะงับบ
 				และรอบนี้เรากันในธีมของรถแห่ ซึ่งเราจะพาน้องๆไปเรียนรู้เรื่องรถและการเขียนโปรแกรมของภาษา C /
 				C++ โดยในค่ายนี้น้องๆจะได้เรียนรู้การเขียนโปรแกรมภาษา C / C++
 				เพื่อเตรียมความรู้พื้นฐานในการเรียนที่คณะวิศวกรรมศาสตร์ สาขาวิศวกรรมคอมพิวเตอร์ ชั้นปีที่ 1
 			</p>
 		</div>
-		<img
-			src="/ceboostup-xi-logo.png"
-			alt="people discussing on board"
-			class="h-2/3 w-2/3 md:w-1/3 md:h-1/3 mt-8"
-		/>
+		<div class="flex justify-center items-center">
+			<img
+				src="/ceboostup-xi-logo.png"
+				alt="people discussing on board"
+				class="w-1/2 h-full object-cover"
+			/>
+		</div>
 	</div>
+
+	<p>
+		"จริงๆ พี่มีเอกสารประกอบการสอนให้นะ แต่เพื่อนมันยังไม่ให้มา 555555" <span
+			class="text-white glass px-2">@1tpp so makmak<span /></span
+		>
+	</p>
 </div>
